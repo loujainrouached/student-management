885767dc68360a8d160578dbbeb1185c18be8129
loujainrouached <loujein.rouached@esprit.tn>
2025-12-04 23:25:25 +0100

    Fix Optional.get() bug in DepartmentServic-enrollment e

--- student-management/src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
+++ student-management/src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
@@ -1,7 +1,5 @@
 package tn.esprit.studentmanagement.services;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.repositories.EnrollmentRepository;
 import tn.esprit.studentmanagement.entities.Enrollment;
@@ -9,7 +7,7 @@ import java.util.List;
 
 @Service
 public class EnrollmentService implements IEnrollment {
-    @Autowired
+
     EnrollmentRepository enrollmentRepository;
 
     @Override
@@ -19,8 +17,10 @@ public class EnrollmentService implements IEnrollment {
 
     @Override
     public Enrollment getEnrollmentById(Long idEnrollment) {
-        return enrollmentRepository.findById(idEnrollment).get();
+        return enrollmentRepository.findById(idEnrollment)
+                .orElseThrow(() -> new RuntimeException("Enrollment not found with id: " + idEnrollment));
     }
+
 
     @Override
     public Enrollment saveEnrollment(Enrollment enrollment) {
