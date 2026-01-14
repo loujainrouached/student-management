e8a54027b9ce8d3511b9c82378de578548287847
ROUACHED Loujein <131523209+loujainrouached@users.noreply.github.com>
2026-01-14 09:42:14 +0100

    Update StudentManagementApplicationTests.java

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,6 +2,7 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue; // ⬅️ IMPORT MANQUANT
 
 @SpringBootTest
 class StudentManagementApplicationTests {
